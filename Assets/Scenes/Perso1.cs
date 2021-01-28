3ff64596bb7b407e09c148dd752a7659eaece8ce
laurent-cortijo <75366986+laurent-cortijo@users.noreply.github.com>
2021-01-28 20:17:55 +0100

    C sharp

---
+++ Assets/Scenes/Perso1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perso1 : MonoBehaviour
+{
+    public GameObject card;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("c") && card.activeSelf)
+        {
+            card.SetActive(false);
+        }
+
+        else if (Input.GetKeyDown("c") && !card.activeSelf)
+        {
+            card.SetActive(true);
+        }
+    }
+}
